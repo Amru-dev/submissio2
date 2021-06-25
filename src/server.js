eb92fd4bc6aeb516d577462f60aa44e782c933cf
Amru Salim <salimamru10@gmail.com>
2021-06-25 16:57:01 +0700

    llll

--- src/server.js
+++ src/server.js
@@ -29,6 +29,10 @@ const collaborations = require('./api/collaborations');
 const CollaborationsService = require('./services/postgres/CollaborationsService');
 const CollaborationsValidator = require('./validator/collaborations');
 
+const _exports = require('./api/exports');
+const ProducerService = require('./services/rabbitmq/ProducerService');
+const ExportsValidator = require('./validator/exports');
+
 const init = async () => {
   const collaborationsService = new CollaborationsService();
   const songsService = new SongsService(collaborationsService);
@@ -114,6 +118,14 @@ const init = async () => {
         collaborationsService,
         playlistsService,
         validator: CollaborationsValidator,
+      },
+    },
+    {
+      plugin: _exports,
+      options: {
+        service: ProducerService,
+        playlistsService,
+        validator: ExportsValidator,
       },
     },
   ]);
